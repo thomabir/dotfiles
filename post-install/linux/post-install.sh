8bdb1d9b91f5f36259a30b1667a8d04bcbfc7f35
Thomas Birbacher <thomas@birbacher.com>
2024-09-26 11:06:12 +0200

    Fix pyenv

--- post-install/linux/post-install.sh
+++ post-install/linux/post-install.sh
@@ -36,6 +36,9 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
 # Python with pyenv
 # instructions: https://github.com/pyenv/pyenv#installation
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
