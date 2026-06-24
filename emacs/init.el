5b6cc998fb89e0124629f6ceeb109186477014cf
Thomas Birbacher <thomas@birbacher.com>
2026-06-24 22:17:23 +0200

    Restore actual Emacs init.el and remove unused early-init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,312 +1,51 @@
-;; Turn off welcome screen
+;;; init.el -*- lexical-binding: t; -*-
+
+(tool-bar-mode -1)
 (setq inhibit-splash-screen t)
+(set-frame-font "Iosevka Term 16" nil t)
+(pixel-scroll-precision-mode 1)
+(global-visual-line-mode 1)
+(load-theme 'leuven t)
 
-(setq initial-major-mode 'fundamental-mode)  ; default mode for the *scratch* buffer
-(setq display-time-default-load-average nil) ; this information is useless for most
+;; Free the right Option key for system Unicode input.
+(setq ns-right-option-modifier 'none)
 
-;; Automatically reread from disk if the underlying file changes
-;; (setq auto-revert-interval 1)
-;; (setq auto-revert-check-vc-info t)
-;; (global-auto-revert-mode)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 
-;; Save history of minibuffer
-;; (savehist-mode)
+;; Tree-sitter (Emacs 31): offer to install missing grammars, and prefer
+;; the *-ts-mode variant whenever a grammar is available.
+(setq treesit-auto-install-grammar 'ask
+      treesit-enabled-modes t
+      treesit-font-lock-level 4)
 
-;; No double space after end of sentence
-(setq sentence-end-double-space nil)
+;; Org.
+(setq org-confirm-babel-evaluate nil
+      org-image-actual-width 900
+      org-startup-indented t
+      org-hide-leading-stars t
+      org-indent-indentation-per-level 1)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell . t)
+   (python . t)))
 
-;; Show the help buffer after startup
-;; (add-hook 'after-init-hook 'help-quick)
-
-;; which-key: shows a popup of available keybinding continuations
 (use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
+  :ensure nil
+  :config (which-key-mode 1))
 
-
-
-;; minibuffer completion
-
-(setq enable-recursive-minibuffers t)                ; Use the minibuffer whilst in the minibuffer
-(setq completion-cycle-threshold 1)                  ; TAB cycles candidates
-(setq completions-detailed t)                        ; Show annotations
-(setq tab-always-indent 'complete)                   ; When I hit TAB, try to complete, otherwise, indent
-(setq completion-styles '(basic initials substring)) ; Different styles to match input to candidates
-
-(setq completion-auto-help 'always)                  ; Open completion always; `lazy' another option
-(setq completions-max-height 20)
-(setq completions-detailed t)
-(setq completions-format 'one-column)
-(setq completions-group t)
-(setq completion-auto-select 'second-tab)            ; Much more eager
-;(setq completion-auto-select t)                     ; See `C-h v completion-auto-select' for more possible values
-
-(keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete) ; TAB acts more like how it does in the shell
-
-
-
-;;;   Interface enhancements/defaults
-
-
-;; Mode line information
-(setq line-number-mode t)                        ; Show current line in modeline
-(setq column-number-mode t)                      ; Show column as well
-
-(setq x-underline-at-descent-line nil)           ; Prettier underlines
-(setq switch-to-buffer-obey-display-actions t)   ; Make switching buffers more consistent
-
-(setq-default show-trailing-whitespace nil)      ; By default, don't underline trailing spaces
-(setq-default indicate-buffer-boundaries 'left)  ; Show buffer top and bottom in the margin
-
-;; Enable horizontal scrolling
-(setq mouse-wheel-tilt-scroll t)
-(setq mouse-wheel-flip-direction t)
-
-;; Misc. UI tweaks
-(blink-cursor-mode -1)                                ; Steady cursor
-(pixel-scroll-precision-mode)                         ; Smooth scrolling
-
-;; Nice line wrapping when working with text
-(add-hook 'text-mode-hook 'visual-line-mode)
-
-;; Modes to highlight the current line with
-(let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
-  (mapc (lambda (hook) (add-hook hook 'hl-line-mode)) hl-line-hooks))
-
-
-;;;   Tab-bar
-
-
-;; Show the tab-bar as soon as tab-bar functions are invoked
-(setq tab-bar-show 0)
-
-
-;;;   Theme
-
-
-(use-package emacs
-  :config
-  (load-theme 'modus-vivendi))
-
-;; Set font
-(set-frame-font "Iosevka Term 12" nil t)
-
-
-;;;   Motion aids
-
+(use-package helm
+  :bind (("M-x"     . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("C-x r b" . helm-filtered-bookmarks)))
 
 (use-package avy
-  :ensure t
-  :demand t
-  :bind (("C-c j" . avy-goto-line)
-         ("s-j"   . avy-goto-char-timer)))
+  :defer t
+  :bind (("C-:"   . avy-goto-char-timer)
+         ("M-g g" . avy-goto-line)
+         ("M-g w" . avy-goto-word-1))
+  :init (setq avy-all-windows t)
+  :config (avy-setup-default))
 
-
-;;;   Embark and Consult
-
-
-;; Consult: Misc. enhanced commands
-(use-package consult
-  :ensure t
-  ;; Other good things to bind: consult-ripgrep, consult-line-multi,
-  ;; consult-history, consult-outline
-  :bind (("C-x b" . consult-buffer) ; orig. switch-to-buffer
-         ("M-y" . consult-yank-pop) ; orig. yank-pop
-         ("C-s" . consult-line))    ; orig. isearch
-  :config
-  ;; Narrowing lets you restrict results to certain groups of candidates
-  (setq consult-narrow-key "<"))
-
-(use-package embark
-  :ensure t
-  :demand t
-  :after avy
-  :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
-  :init
-  ;; Add the option to run embark when using avy
-  (defun bedrock/avy-action-embark (pt)
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (select-window
-       (cdr (ring-ref avy-ring 0))))
-    t)
-
-  ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
-  ;; candidate you select
-  (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
-
-(use-package embark-consult
-  :ensure t)
-
-
-;;;   Minibuffer and completion
-
-
-;; Vertico: better vertical completion for minibuffer commands
-(use-package vertico
-  :ensure t
-  :init
-  ;; You'll want to make sure that e.g. fido-mode isn't enabled
-  (vertico-mode))
-
-(use-package vertico-directory
-  :after vertico
-  :bind (:map vertico-map
-              ("M-DEL" . vertico-directory-delete-word)))
-
-;; Marginalia: annotations for minibuffer
-(use-package marginalia
-  :ensure t
-  :config
-  (marginalia-mode))
-
-;; Popup completion-at-point
-(use-package corfu
-  :ensure t
-  :init
-  (global-corfu-mode)
-  :bind
-  (:map corfu-map
-        ("SPC" . corfu-insert-separator)
-        ("C-n" . corfu-next)
-        ("C-p" . corfu-previous)))
-
-;; Part of corfu
-(use-package corfu-popupinfo
-  :after corfu
-  :hook (corfu-mode . corfu-popupinfo-mode)
-  :custom
-  (corfu-popupinfo-delay '(0.25 . 0.1))
-  (corfu-popupinfo-hide nil)
-  :config
-  (corfu-popupinfo-mode))
-
-;; Make corfu popup come up in terminal overlay
-(use-package corfu-terminal
-  :if (not (display-graphic-p))
-  :ensure t
-  :config
-  (corfu-terminal-mode))
-
-;; Pretty icons for corfu
-(use-package kind-icon
-  :if (display-graphic-p)
-  :ensure t
-  :after corfu
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-(use-package eshell
-  :bind (("C-r" . consult-history)))
-
-;; Orderless: powerful completion style
-(use-package orderless
-  :ensure t
-  :config
-  (setq completion-styles '(orderless)))
-
-
-
-;;;   Built-in config for development
-(use-package emacs
-  :config
-  ;; Treesitter config
-
-  ;; Tell Emacs to prefer the treesitter mode
-  ;; You'll want to run the command `M-x treesit-install-language-grammar' before editing.
-  (setq major-mode-remap-alist
-        '((yaml-mode . yaml-ts-mode)
-          (bash-mode . bash-ts-mode)
-          (python-mode . python-ts-mode)))
-  :hook
-  ;; Auto parenthesis matching
-  ((prog-mode . electric-pair-mode)))
-
-
-;;; Magit
-(use-package magit
-  :ensure t
-  :bind (("s-g" . magit-status)
-         ("C-c g" . magit-status)))
-
-
-;;;  Markdown 
-(use-package markdown-mode
-  :hook ((markdown-mode . visual-line-mode)))
-
-
-;;;   Eglot
-
-
-(use-package eglot
-  ;; no :ensure t here because it's built-in
-
-  ;; Configure hooks to automatically turn-on eglot for selected modes
-  ; :hook
-  ; (((python-mode ruby-mode elixir-mode) . eglot))
-
-  :custom
-  (eglot-send-changes-idle-time 0.1)
-
-  :config
-  (fset #'jsonrpc--log-event #'ignore)  ; massive perf boost---don't log every event
-  ;; Sometimes you need to tell Eglot where to find the language server
-  ; (add-to-list 'eglot-server-programs
-  ;              '(haskell-mode . ("haskell-language-server-wrapper" "--lsp")))
-  )
-
-
-;;;   Tree sitter
-
-
-(setq treesit-language-source-alist
-   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (make "https://github.com/alemuller/tree-sitter-make")
-     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-
-;;;   evil-mode
-
-
-(use-package evil
-  :ensure t
-
-  :init
-  (setq evil-respect-visual-line-mode t)
-  (setq evil-undo-system 'undo-redo)
-
-  ;; Enable this if you want C-u to scroll up, more like pure Vim
-  ;(setq evil-want-C-u-scroll t)
-
-  :config
-  (evil-mode)
-
-  ;; Configuring initial major mode for some modes
-  (evil-set-initial-state 'vterm-mode 'emacs))
-
-
-;;;   Built-in customization framework
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file) (load custom-file))
